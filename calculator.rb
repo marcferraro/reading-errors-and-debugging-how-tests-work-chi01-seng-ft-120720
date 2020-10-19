435add4bc077ebd61eb02d83b9345d06209285e1
Marc Ferraro <marc.ferraro1@gmail.com>
2020-10-19 19:04:56 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,4 +1,12 @@
 first_number = 2
 second_number = 3
+
 sum = first_number + second_number
 puts sum
+
+difference = first_number - second_number
+puts difference
+
+quotient = first_number * second_number
+puts quotient
+
