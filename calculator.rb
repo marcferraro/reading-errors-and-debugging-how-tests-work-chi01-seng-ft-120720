3dec5a290e961a252e2a3eb03c5f8763510360f8
Marc Ferraro <marc.ferraro1@gmail.com>
2020-10-19 19:08:52 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,14 @@
-# Add your variables here
+first_number = 2.0
+second_number = 3.0
+
+sum = first_number + second_number
+puts sum
+
+difference = first_number - second_number
+puts difference
+
+product = first_number * second_number
+puts product
+
+quotient = first_number / second_number
+puts quotient
